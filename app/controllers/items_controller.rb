3aaae0d5a65b41f4ec99746bf509e5ac0b56441b
naoki39 <naokibad0311@gmail.com>
2022-02-10 17:09:24 +0900

    itemsコントローラーのアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :state_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: currnt_user.id )
+  end
 end
