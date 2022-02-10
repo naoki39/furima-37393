b2b6a58561c04c225fa3f65677d009f87e0aad87
naoki39 <naokibad0311@gmail.com>
2022-02-10 16:56:22 +0900

    shipping_dayモデルの作成とitemモデルのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,17 @@ class Item < ApplicationRecord
   belongs_to :state
   belongs_to :delivery_charge
   belongs_to :shipping_area
+  belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :nickname
+    validates :info
+    validates :price, format: { with: /\A[0-9]+\z/ }
+  end
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :state_id, numericality: { other_than: 1 }
+  validates :delivery_charge_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
 end
