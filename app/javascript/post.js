ab1a990887690b63041e1edb778d36995b8ada12
naoki39 <naokibad0311@gmail.com>
2022-02-11 11:06:46 +0900

    post.jsの埋め込み

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,5 +1,4 @@
-function post (){
-  console.log("非同期投稿を実装");
-};
-
-window.addEventListener('losd', post)
+window.addEventListener('losd', function(){
+  const pullDownButton = document.getElementById("item-category")
+  const pullDownParents = document.getElementById("")
+}
