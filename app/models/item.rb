5ff6581ed6cfccfa2e1cedd0fecfcc6278a622d8
naoki39 <naokibad0311@gmail.com>
2022-02-12 14:35:12 +0900

    itemsモデルのテストコード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,14 +5,14 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   belongs_to :shipping_area
   belongs_to :shipping_day
+  belongs_to :user
   has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :info
     validates :image
-    validates :price, format: { with: /\A[0-9]+\z/ },
-                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   validates :category_id, numericality: { other_than: 1 }
