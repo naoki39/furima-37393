33a887194cdf8a6d84de93678c87b70746d10f1b
naoki39 <naokibad0311@gmail.com>
2022-02-18 15:32:50 +0900

    クレジット決済機能の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,16 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :shipping_area_id, :municipalities, :house_number, :building, :telephone_number, :item_id, :user_id,:order_id
+  attr_accessor :post_code, :shipping_area_id, :municipalities, :house_number, :building, :telephone_number, :item_id, :user_id, :order_id, :token
 
-  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :municipalities
+    validates :house_number
+    validates :telephone_number, numericality: {only_integer: true, }, format: {with: /[0-9]{10,11}/ }
+    validates :token
+  end
+
   validates :shipping_area_id, numericality: { other_than: 1 }
-  validates :municipalities, presence: true
-  validates :house_number, presence: true
-  validates :telephone_number, presence: true, numericality: {only_integer: true, }, format: {with: /[0-9]{10,11}/ }
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe OrderAddress, type: :model do
   
     describe '商品購入' do
       context '商品購入ができる場合' do
-        it 'post_codeとshipping_area_idとmunicipalitiesとhouse_numberとtelephone_numberとorder_idが存在すれば購入できる' do
+        it 'post_codeとshipping_area_idとmunicipalitiesとhouse_numberとtelephone_numberとorder_idとtokenが存在すれば購入できる' do
           expect(@order_address).to be_valid
         end
       end
@@ -48,6 +48,11 @@ RSpec.describe OrderAddress, type: :model do
           @order_address.telephone_number = '12345678'
           @order_address.valid?
           expect(@order_address.errors.full_messages).to include("Telephone number is invalid")
+        end
+        it "tokenが空では購入できない" do
+          @order_address.token = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Token can't be blank")
         end
       end
     end
